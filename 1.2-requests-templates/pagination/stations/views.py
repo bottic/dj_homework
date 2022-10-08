4877ad0cfdba469caa7280f8d7dc0c65801eb56a
Артём <tyomych.kot@gmail.com>
2022-10-08 13:52:11 +0300

    Недоработанная пагинация

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,9 @@
+import csv
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.conf import settings
 
 
 def index(request):
@@ -7,11 +11,16 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+    station = []
+    with open(settings.BUS_STATION_CSV, newline='') as csvfile:
+        station_dictreader = csv.DictReader(csvfile)
+        station = [row for row in station_dictreader]
+    page_number = int(request.GET.get('page', 1))
+    paginator = Paginator(station, 1)
+
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': station,
+        'page': paginator.get_page(page_number),
     }
     return render(request, 'stations/index.html', context)
