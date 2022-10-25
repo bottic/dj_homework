fda98934d8b3deb488828cc0dc824da5569fb03a
Артём <tyomych.kot@gmail.com>
2022-10-25 14:23:05 +0300

    do m2m

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
+from .models import Article, Scope, Teg
 
-from .models import Article
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            cl_data = form.cleaned_data
+            if cl_data != {}:
+                if cl_data['is_main']:
+                    count += 1
+        if count > 1:
+            raise ValidationError('Значение  is_main > 1')
+        elif count == 0:
+            raise ValidationError('Значение  is_main = 0')
+
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+
+
+@admin.register(Teg)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'text']
+    list_filter = ['title']
+    inlines = [ScopeInline, ]
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -11,6 +11,26 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Teg(models.Model):
+    title = models.TextField(max_length=50)
+    article = models.ManyToManyField(Article, related_name='tegs', through='Scope')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.title
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Teg, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField()
+
